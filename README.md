eb150c873d99620c95261c79664f54353b6ccf3a
ANTONIO ERISVAN ALVES JUNIOR <eris.junior@A102-03L.imd.local>
2019-07-24 16:09:38 -0300

    Init firstProject and README

---
+++ README.md
@@ -0,0 +1,24 @@
+# Começa
+## Que
+### Aqui
+
+- [x] Task One
+- [ ] Task Two
+
+- Isso
+- Por
+- Aí
+
+[eris.junior](https://github.com)
+
+```
+git eris
+git junior
+```
+
+> aqui é `eris` junior
+
+**juniioooor**
+_juniioooor_
+
+@github/support A :+1: 
---
+++ class_one/transformIntoFeet.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* Esse programa
+vai ser lindo
+porém vai sair lix...
+ops, windows
+
+Converte Metro em Pés
+*/
+
+int main() {
+
+    //Definir variaveis
+    int libras;
+    float pes;
+    double kg, metros;
+
+    //Ler entrada
+
+    //Faz a conta
+
+    //Imprime resultado
+
+    return 0;
+}
